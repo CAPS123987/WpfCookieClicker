244c24625c2f78cdd5bd370da8faf7c623524837
jakub.hnilicka <jakub.hnilicka@PC-U13-33>
2026-05-19 08:37:28 +0200

    Přidejte soubory projektu.

---
+++ CookieClicker/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookieClicker
+{
+    public class Game
+    {
+        private double Cookies { get; set; }
+        private double CookiesPerClick { get; set; }
+        private double CookiesPerSecond { get; set; }
+        private int TotalClicks { get; set; }
+
+        /**
+         * @returns amount of cookies given
+         */
+        public double handleClick()
+        {
+            this.Cookies += this.CookiesPerClick;
+            return this.CookiesPerClick;
+        }
+    }
+}
